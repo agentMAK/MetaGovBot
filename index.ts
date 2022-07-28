e91a2dcbeb4576197a6f9c3495180e19705ee9eb
agentMAK <martin0044@gmail.com>
2022-07-28 16:17:31 +0100

    Troubleshooting

--- index.ts
+++ index.ts
@@ -10,9 +10,12 @@ app.use(express.json());
 app.post("/" ,(req, res) => {
     const event = req.body
 
+    console.log("received")
+
     if(event.event == "proposal/created") {
         if(process.env.WATCHED_SPACES.split(",").includes(event.space)){
             const id = event.id.substring(9)
+            console.log(id)
             proposalEvents.emit('Proposal Created',id);
 
         }
@@ -31,7 +34,7 @@ const proposalEvents = new events.EventEmitter();
 
 proposalEvents.on('Proposal Created', async (id) => {
     const proposal = await getProposalById(id);
-    //console.log(proposal)
+    console.log(proposal)
     postToSnapshot(proposal);
 });
 
