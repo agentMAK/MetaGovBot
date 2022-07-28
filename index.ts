76e45c770deacca5c6bf4b26e3c611dc5c96034a
agentMAK <martin0044@gmail.com>
2022-07-28 17:00:47 +0100

    Add logs

--- index.ts
+++ index.ts
@@ -32,7 +32,7 @@ const proposalEvents = new events.EventEmitter();
 
 proposalEvents.on('Proposal Created', async (id) => {
     const proposal = await getProposalById(id);
-    console.log(proposal)
+    //console.log(proposal)
     //postToSnapshot(proposal);
 });
 
--- utils/graphQL.ts
+++ utils/graphQL.ts
@@ -6,6 +6,8 @@ import fetch from 'cross-fetch';
 axiosRetry(axios, { retries: 3 });
 
 export async function getProposalById(id) {
+
+  console.log("fetch Method")
     
     const data = JSON.stringify({
         query: `query Proposal ($proposalId: String!){
@@ -39,6 +41,8 @@ export async function getProposalById(id) {
       };
 
       const response = await axios(config as unknown)
+      console.log(response)
+      console.log("fetch finished")
       return response.data.data.proposal
 
 }
